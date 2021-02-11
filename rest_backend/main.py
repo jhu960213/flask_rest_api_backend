64af4133891607b2943e26069042c7794815c77f
jhu960213 <jingxujaster@hotmail.com>
2021-02-11 01:05:31 -0500

    added more functionality to my rest api

--- rest_backend/main.py
+++ rest_backend/main.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask import jsonify
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)  # flask app instantiation
 api = Api(app)  # wrap our flask app in a API (which we are currently using REST api)
+
+# create a new request parser object so it can help us parse our HTTP request args when invoking our API
+req_parser = reqparse.RequestParser()
+
+# add arguments to parse for
+req_parser.add_argument("name", type=str, help="Please specify a name for the video", required=True)
+req_parser.add_argument("likes", type=int, help="Please specify how many likes for the video", required=True)
+req_parser.add_argument("views", type=int, help="Please specify how many views for the video", required=True)
 
 # define a dictionary of information to be returned
 names_dict = {'jack': {'age': 25, 'gender': 'male'},
@@ -19,12 +27,10 @@ videos_dict = {}
 class MyResource(Resource):
 
     # HTTP GET request
-    @staticmethod
     def get(self, name):
         return jsonify(names_dict[str(name)])
 
     # HTTP POST request
-    @staticmethod
     def post(self):
         return jsonify({'data': "posted!!!"})
 
@@ -32,16 +38,42 @@ class MyResource(Resource):
 # another resource.... for video
 class Video(Resource):
 
-    @staticmethod
+    # returning an item back to client
     def get(self, video_id):
+        self._abort_if_not_exists(video_id=video_id)
         return videos_dict[video_id]
+
+    # can specify a response code along with the sent data, HTTP 201 = data entry has been created
+    def put(self, video_id):
+        self._abort_if_exists(video_id=video_id)
+        parsed_args = req_parser.parse_args()
+        videos_dict[video_id] = parsed_args
+        return videos_dict[video_id], 201
+
+    # delete an entry in data base, if successful then HTTP 204 = delete successfully
+    def delete(self, video_id):
+        self._abort_if_not_exists(video_id=video_id)
+        deleted = videos_dict[video_id]
+        del videos_dict[video_id]
+        return deleted, 204
+
+    # aborting if item already exists HTTP 409 = conflict
+    def _abort_if_exists(self, video_id):
+        if video_id in videos_dict:
+            abort(409, message="Video already exists!")
+
+    # will abort the HTTP request if video id does not exits HTTP 404 = not valid or not found
+    def _abort_if_not_exists(self, video_id):
+        if video_id not in videos_dict:
+            abort(404, message="Video id not valid...")
+
 
 
 # adding resources my rest api
 # essentially determining what the route is going to be for my added resource
 # this is a way to pass params in my http requests "/hello<string:name>/<int:age>"
 api.add_resource(MyResource, "/hello/<string:name>")  # in this case the route is "localhost/hello"
-api.add_resource(Video, "/video/<int:video_id>") # video end point with 1 param
+api.add_resource(Video, "/video/<int:video_id>")  # video end point with 1 param
 
 if __name__ == "__main__":
     app.run(debug=True)  # starts a localhost flask webserver debug is true to help you see the errors if there's any
--- rest_backend/test_rest_api.py
+++ rest_backend/test_rest_api.py
@@ -3,15 +3,47 @@ import requests
 """
 Note: this python script shall test the behaviour of my rest api by sending mock HTTP requests to restful api
 """
+# define test arguments
+data = [{"likes": 12321, "name": "hello", "views": 32400000},
+        {"likes": 50, "name": "jack", "views": 10000000000},
+        {"likes": 500000, "name": "Step Sister Fantasy", "views": 10000000000},
+        {"likes": 500000, "name": "Riley Ried", "views": 10000000000}]
 
 # define base api url and respective resource end points
-name = "jack"
 BASE_URL = "http://127.0.0.1:5000/"
-end_point = f"hello/{name}"
+name_hello = "jack"
+end_point_hello = f"hello/{name_hello}"
+video_id = 1
+end_point_video = f"video/{video_id}"
 
-# define all HTTP requests here to evaluate functional correctness of api
-get_response = requests.get(f"{BASE_URL}{end_point}")
-# post_response = requests.post(f"{BASE_URL}{end_point}")
-print(get_response.json())
-# print(post_response.json())
+# send some requests
+for i in range(0, len(data)):
+    response = requests.put(f"{BASE_URL}video/{i}", data[i])
+    print(response.json())
 
+# delete some items from our videos_dict
+response = requests.delete(f"{BASE_URL}video/{6}")
+print(response.json())
+response = requests.delete(f"{BASE_URL}video/{1}")
+print(response)
+
+# try to retrieve some items from our data base
+response = requests.get(f"{BASE_URL}video/{0}")
+print(response.json())
+
+
+
+
+
+# # define all HTTP requests here to evaluate functional correctness of api
+# get_response = requests.get(f"{BASE_URL}{end_point_hello}")
+# # post_response = requests.post(f"{BASE_URL}{end_point}")
+#
+# put_response_video = requests.put(f"{BASE_URL}{end_point_video}", test_args)
+# get_response_video = requests.get(f"{BASE_URL}video/{2}")
+#
+# print(get_response.json())
+# # print(post_response.json())
+#
+# print(put_response_video.json())
+# print(get_response_video.json())
