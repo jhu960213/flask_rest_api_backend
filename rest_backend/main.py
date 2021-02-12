319b94e29c7a0660e9488003c7c9942a33adc6b4
jhu960213 <jingxujaster@hotmail.com>
2021-02-12 01:22:03 -0500

    added data base rest api now works with sql database

--- rest_backend/main.py
+++ rest_backend/main.py
@@ -1,24 +1,51 @@
 from flask import Flask
 from flask import jsonify
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)  # flask app instantiation
 api = Api(app)  # wrap our flask app in a API (which we are currently using REST api)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # specifies the path of our connected database
+db = SQLAlchemy(app)  # fit our flask with with a SQL database
+
+
+# create our model of our data base
+class VideoModel(db.Model):
+    # columns of our database
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)  # string can not be null and max at 100 chars
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    # be able to print our our data base
+    def __repr__(self):
+        return f"Video(name={self.name}, views={self.views}, likes={self.likes})"
+
+
+# db.create_all()  # creates and initializes the data base. This should only be called ONCE after we specify our
+# model!!!
 
 # create a new request parser object so it can help us parse our HTTP request args when invoking our API
 req_parser = reqparse.RequestParser()
+update_parser = reqparse.RequestParser()
 
 # add arguments to parse for
 req_parser.add_argument("name", type=str, help="Please specify a name for the video", required=True)
 req_parser.add_argument("likes", type=int, help="Please specify how many likes for the video", required=True)
 req_parser.add_argument("views", type=int, help="Please specify how many views for the video", required=True)
 
+# add arguments to updated parser
+update_parser.add_argument("name", type=str, help="Please specify a name for the video")
+update_parser.add_argument("likes", type=int, help="Please specify how many likes for the video")
+update_parser.add_argument("views", type=int, help="Please specify how many views for the video")
+
 # define a dictionary of information to be returned
 names_dict = {'jack': {'age': 25, 'gender': 'male'},
               'ben': {'age': 21, 'gender': 'male'}}
 
-# video dict
-videos_dict = {}
+
+# # video dict
+# videos_dict = {}
 
 
 # creating a resource here, usually a class that inherits the Resource class where we will be able to overwrite
@@ -35,38 +62,68 @@ class MyResource(Resource):
         return jsonify({'data': "posted!!!"})
 
 
+# defines how we want instances of our video model to be serialized in JSON objects
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
+
+
 # another resource.... for video
 class Video(Resource):
 
-    # returning an item back to client
+    # returning an video back to client from data base by serializing following a specific format
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        self._abort_if_not_exists(video_id=video_id)
-        return videos_dict[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()  # queries by id column returns the first matching case
+        if not result:
+            abort(404, message="Could not find video with that id...")
+        return result
 
     # can specify a response code along with the sent data, HTTP 201 = data entry has been created
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        self._abort_if_exists(video_id=video_id)
         parsed_args = req_parser.parse_args()
-        videos_dict[video_id] = parsed_args
-        return videos_dict[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken.....")
+        video = VideoModel(id=video_id, name=parsed_args['name'],
+                           views=parsed_args['views'], likes=parsed_args['likes'])
+        db.session.add(video)  # temp add video to data base
+        db.session.commit()  # commit the added video to the data base for persistence
+        return video, 201
 
-    # delete an entry in data base, if successful then HTTP 204 = delete successfully
-    def delete(self, video_id):
-        self._abort_if_not_exists(video_id=video_id)
-        deleted = videos_dict[video_id]
-        del videos_dict[video_id]
-        return deleted, 204
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        parsed_args = update_parser.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video does not exist, cannot update....")
+        if parsed_args['name']:
+            result.name = parsed_args['name']
+        if parsed_args['likes']:
+            result.likes = parsed_args['likes']
+        if parsed_args['views']:
+            result.views = parsed_args['views']
+        db.session.commit()
+        return result
 
-    # aborting if item already exists HTTP 409 = conflict
-    def _abort_if_exists(self, video_id):
-        if video_id in videos_dict:
-            abort(409, message="Video already exists!")
+    # # delete an entry in data base, if successful then HTTP 204 = delete successfully
+    # def delete(self, video_id):
+    #
+    #     return deleted, 204
 
-    # will abort the HTTP request if video id does not exits HTTP 404 = not valid or not found
-    def _abort_if_not_exists(self, video_id):
-        if video_id not in videos_dict:
-            abort(404, message="Video id not valid...")
-
+    # # aborting if item already exists HTTP 409 = conflict
+    # def _abort_if_exists(self, video_id):
+    #     if video_id in videos_dict:
+    #         abort(409, message="Video already exists!")
+    #
+    # # will abort the HTTP request if video id does not exits HTTP 404 = not valid or not found
+    # def _abort_if_not_exists(self, video_id):
+    #     if video_id not in videos_dict:
+    #         abort(404, message="Video id not valid...")
 
 
 # adding resources my rest api
--- rest_backend/test_rest_api.py
+++ rest_backend/test_rest_api.py
@@ -21,19 +21,18 @@ for i in range(0, len(data)):
     response = requests.put(f"{BASE_URL}video/{i}", data[i])
     print(response.json())
 
-# delete some items from our videos_dict
-response = requests.delete(f"{BASE_URL}video/{6}")
-print(response.json())
-response = requests.delete(f"{BASE_URL}video/{1}")
-print(response)
+# # delete some items from our videos_dict
+# response = requests.delete(f"{BASE_URL}video/{6}")
+# print(response.json())
+# response = requests.delete(f"{BASE_URL}video/{1}")
+# print(response)
 
 # try to retrieve some items from our data base
-response = requests.get(f"{BASE_URL}video/{0}")
+response = requests.get(f"{BASE_URL}video/{4}")
 print(response.json())
-
-
-
-
+input()
+response = requests.patch(f"{BASE_URL}video/{1}", data={})
+print(response.json())
 
 # # define all HTTP requests here to evaluate functional correctness of api
 # get_response = requests.get(f"{BASE_URL}{end_point_hello}")
